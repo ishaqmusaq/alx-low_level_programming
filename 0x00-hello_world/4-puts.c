1a2f9207e4294df94cc186cafa4cd19233974a66
ishaqmusaq <0264410035ishaq@gmail.com>
2022-06-30 23:45:31 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-#include  <stdio.h> int main(void) {puts("\"programming is like building a multilingual puzzle"); return (0); }
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print word to output
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
